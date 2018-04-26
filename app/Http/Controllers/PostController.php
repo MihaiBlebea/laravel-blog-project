f78964758e8279962c208bbd82d76764af06d1a0
Serban Blebea <slabestecuserban@gmail.com>
2018-04-27 01:15:22 +0300

    Add crud methods for Post model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,31 +4,56 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Auth;
+use App\Http\Requests\PostFormRequest;
 
 class PostController extends Controller
 {
     public function index()
     {
-
+        return Post::all();
     }
 
     public function get(Post $post)
     {
-        dd($post);
+        return $post;
     }
 
-    public function store()
+    public function store(PostFormRequest $request)
     {
-        //
+        $post = Post::create([
+            'category_id'   => $request->input('category_id'),
+            'user_id'       => Auth::user()->id,
+            'slug'          => str_slug($request->input('title'), '-'),
+            'title'         => $request->input('title'),
+            'feature_image' => $request->input('feature_image'),
+            'content'       => $request->input('content'),
+        ]);
+
+        if($post)
+        {
+            return redirect()->back();
+        }
     }
 
     public function update(Post $post)
     {
-        //
+        $post->update([
+            'category_id'   => $request->input('category_id'),
+            'slug'          => str_slug($request->input('title'), '-'),
+            'title'         => $request->input('title'),
+            'feature_image' => $request->input('feature_image'),
+            'content'       => $request->input('content'),
+        ]);
     }
 
     public function delete(Post $post)
     {
-
+        $comments = $post->comments;
+        foreach($comments as $comment)
+        {
+            $comment->delete();
+        }
+        $post->delete();
     }
 }
