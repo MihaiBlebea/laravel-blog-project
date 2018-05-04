14fde6cf4cdd0edfb6df4ddc03996ed9bbaeee5a
Serban Blebea <slabestecuserban@gmail.com>
2018-05-04 22:24:44 +0100

    Add cover image to category card

--- resources/views/partials/_category-card.blade.php
+++ resources/views/partials/_category-card.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <a href="{{ route('blog.category', [ 'category' => $category->slug ]) }}">
-        <img class="card-img-top" src="https://s3-img.pixpa.com/local/800/dummy-image.jpg" alt="Card image cap">
+        <img class="card-img-top" src="{{ public_upload_path($category->cover_image) }}" alt="cover image">
     </a>
     <div class="card-body">
         <h5 class="card-title">{{ $category->name }}</h5>
