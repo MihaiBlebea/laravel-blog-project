7c97aa2422c7f29b28d4069e4ac3dbcf8aa7389f
Mihai Blebea <mblebea@tfspark.com>
2018-08-23 23:25:16 +0100

    fix the card error where image is null, added default null value

--- resources/views/partials/cards/_category-card.blade.php
+++ resources/views/partials/cards/_category-card.blade.php
@@ -1,8 +1,8 @@
 <div class="card">
     <a href="{{ route('blog.category', [ 'category' => $category->slug ]) }}">
         {{-- <img style="max-width:100%" src="{{ asset($category->image->path ?? 'images/post-placeholder.png') }}"> --}}
-        @include('partials._image-responsive', [ 'image_path' => $category->image->path ])
-        
+        @include('partials._image-responsive', [ 'image_path' => $category->image->path ?? null ])
+
         <h3 class="card-title card-title--overlay card-title--overlay__white p-2 mb-0">
             {{ $category->name }}
         </h3>
--- resources/views/partials/cards/_category-description-card.blade.php
+++ resources/views/partials/cards/_category-description-card.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
     {{-- <img class="profile-image-wrapper mr-2 ml-3" src="{{ asset($category->image->path ?? 'images/profile-placeholder.jpg') }}"> --}}
     <div class="col-md-4 col-12 mb-3">
-        @include('partials._image-responsive', ['image_path' => $category->image->path])
+        @include('partials._image-responsive', ['image_path' => $category->image->path ?? null])
     </div>
 
     <div class="col">
