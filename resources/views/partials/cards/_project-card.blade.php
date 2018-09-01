be4b5f1a2d1b6f0975f3675244e78abc8afed1a4
Serban Blebea <slabestecuserban@gmail.com>
2018-09-01 22:16:32 +0100

    fix project card

--- resources/views/partials/cards/_project-card.blade.php
+++ resources/views/partials/cards/_project-card.blade.php
@@ -3,7 +3,7 @@
         <div class="image--bg image--bg__medium w-100"
              style="background-image: url('{{ asset($project->images[0]->path ?? 'images/post-placeholder.png') }}');">
         </div>
-        <h3 class="card-title card-title--overlay card-title--overlay__white p-2 mb-0">
+        <h3 class="card__title card__title--white p-2 mb-0">
             {{ $project->name }}
         </h3>
     </a>
