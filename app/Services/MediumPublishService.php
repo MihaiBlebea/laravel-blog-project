df7ffbe98f92df1121c8b0fc5113319982db98db
Serban Blebea <slabestecuserban@gmail.com>
2018-09-02 18:47:07 +0100

    Add image to Medium post

--- app/Services/MediumPublishService.php
+++ app/Services/MediumPublishService.php
@@ -13,13 +13,34 @@ class MediumPublishService implements MediumPublishServiceInterface
     {
         $medium->connect(env('MEDIUM_TOKEN'));
         $user = $medium->getAuthenticatedUser();
+
+        $uploaded_image_path = self::uploadImage($medium, $post);
+
         $data = [
             'title'         => $post->title,
             'contentFormat' => 'markdown',
-            'content'       => $post->content,
+            'content'       => self::concatMainImage($post->content, $uploaded_image_path),
             'publishStatus' => 'public',
         ];
 
         return $medium->createPost($user->data->id, $data);
+    }
+
+    private static function uploadImage(Medium $medium, Post $post)
+    {
+        $image = fopen($post->image->path, 'r');
+        $uploaded_image = $medium->uploadImage($image, 'main-image.png');
+
+        return $uploaded_image->data->url;
+    }
+
+    private static function concatMainImage(String $content, String $image_path)
+    {
+        return self::createMarkdownImage($image_path) . " \r\n " . $content;
+    }
+
+    private static function createMarkdownImage(String $image_path)
+    {
+        return '![main image](' . $image_path . ')';
     }
 }
